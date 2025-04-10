cba5344737b0d0f1d8351886f89da9010c46d8db
dlond <dlond@me.com>
2025-04-10 14:21:09 +1200

    hid flake.nix

---
+++ .env/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Python development environment using Nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "aarch64-darwin";
+    pkgs = import nixpkgs { inherit system; };
+
+    pythonEnv = pkgs.python313.withPackages (ps: with ps; [
+      numpy
+      pandas
+      scipy
+      matplotlib
+      ipython
+    ]);
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pythonEnv
+      ];
+
+      shellHook = ''
+        export VIRTUAL_ENV=$PWD/.nix-python-env
+        export PATH=$VIRTUAL_ENV/bin:$PATH
+        echo "Activated Nix flake Python environment"
+
+        exec zsh --rcs
+      '';
+    };
+  };
+}
